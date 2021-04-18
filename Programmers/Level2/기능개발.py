5a76766d0ab125d1975f61a0177acf71f08380f5
Jihyun503 <s2017s17@e-mirim.hs.kr>
2021-04-19 00:05:07 +0900

    기능개발 수정완료

--- Programmers/Level2/기능개발.py
+++ Programmers/Level2/기능개발.py
@@ -1,10 +1,23 @@
 def solution(progresses, speeds):
     answer = []
-    
     for i, p in enumerate(progresses):
         chk = 0
         while p < 100:
             p += speeds[i]
             chk += 1
+
+        if i == 0:
+            temp = 1
+            min = chk
+        else:
+            if min >= chk:
+                temp += 1
+            else:
+                answer.append(temp)
+                temp = 1
+                min = chk
+
+            if i+1 == len(progresses) :
+                answer.append(temp)
 
     return answer
